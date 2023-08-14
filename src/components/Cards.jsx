3c75a92eab39033a26a677ee62bc8aa029bdc9c1
Esteban Smith Mutis <smithmutis21@gmail.com>
2023-08-13 23:42:09 -0500

    enhancing animations

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -23,8 +23,13 @@ function CardGrid() {
       {cards.map((card) => (
         <motion.figure
           initial={{ opacity: 0 }}
-          whileInView={{ opacity: 1 }}
-          viewport={{ once: true }}
+          whileInView={{
+            opacity: 1,
+            transition: {
+              delay: 0.1,
+            },
+          }}
+          viewport={{ once: true, amount: 0.1 }}
           key={card.id}
           className="max-w-2xl py-6 md:py-12 px-4 md:px-8 bg-third-600 [&>img]:hover:brightness-110 rounded-lg overflow-hidden shadow-lg hover:shadow-2xl transition-all duration-300 flex flex-col justify-between"
         >
