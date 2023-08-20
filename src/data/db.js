dd821962f2e69b455648363bec1cab11aa2467c9
Esteban Smith Mutis <smithmutis21@gmail.com>
2023-08-20 18:34:38 -0500

    testing new logo

--- src/data/db.js
+++ src/data/db.js
@@ -5,33 +5,27 @@ export const db = [
     propuestas: [
       {
         id: 'propuesta1',
-        title: 'Apoyo al Emprendimiento Joven',
-        subtitle: 'Fomentar el emprendimiento entre los jóvenes',
-        description: 'Implementaremos programas de capacitación, asesoría y financiamiento para jóvenes emprendedores, con el objetivo de impulsar sus ideas y proyectos innovadores.',
+        title: 'Capacitación a los impuestos tributarios',
+        subtitle: 'La capacitación es la clave para el éxito de los impuestos tributarios',
+        description: 'Trabajaremos en la capacitación de los impuestos tributarios, creando lugares seguros y atractivos donde los jóvenes puedan desarrollar actividades recreativas, deportivas y culturales.',
       },
       {
         id: 'propuesta2',
-        title: 'Mejora de Espacios Públicos',
-        subtitle: 'Creación y revitalización de áreas para el disfrute de todos',
-        description: 'Trabajaremos en la recuperación y adecuación de espacios públicos, creando lugares seguros y atractivos donde los jóvenes puedan desarrollar actividades recreativas, deportivas y culturales.',
-      },
-      {
-        id: 'propuesta3',
-        title: 'Apoyo al Pequeño Comercio',
+        title: 'Mejoramiento de la participación inclusiva de las organizaciones',
         subtitle: 'Impulsar el crecimiento de los comercios locales',
         description: 'Promoveremos programas de capacitación en marketing, gestión empresarial y transformación digital para pequeños comerciantes, además de facilitar espacios para ferias y exposiciones de productos locales.',
       },
       {
-        id: 'propuesta4',
-        title: 'Modernización del Transporte Público',
-        subtitle: 'Mejorar las condiciones de trabajo de los taxistas',
-        description: 'Trabajaremos en la modernización y regulación del transporte público, incluyendo mejoras en la calidad de los vehículos, acceso a tecnologías de navegación y capacitación en atención al cliente para los taxistas.',
+        id: 'propuesta3',
+        title: 'Control a la gestión pública para el desarrollo local',
+        subtitle: 'Así como los comercios, los emprendimientos locales también necesitan apoyo',
+        description: 'Impulsaremos programas de capacitación y financiamiento para emprendedores locales, además de facilitar espacios para la realización de eventos culturales y artísticos en la comunidad.',
       },
       {
-        id: 'propuesta5',
-        title: 'Apoyo a la Cultura',
-        subtitle: 'Promover la cultura y el arte en la comunidad',
-        description: 'Impulsaremos programas de capacitación y financiamiento para artistas locales, además de facilitar espacios para la realización de eventos culturales y artísticos en la comunidad.',
+        id: 'propuesta4',
+        title: 'Vinculación social y empresarial',
+        subtitle: 'De esta manera, los jóvenes podrán desarrollar sus habilidades y talentos',
+        description: 'Trabajaremos en la capacitación de los jóvenes, creando lugares seguros y atractivos donde los jóvenes puedan desarrollar actividades recreativas, deportivas y culturales.',
       },
     ]
   }
