288c4eb6b5958ecc85b9922106adac3573e707a2
Esteban Smith Mutis <smithmutis21@gmail.com>
2023-08-21 00:13:47 -0500

    testing new images

--- src/data/bg.js
+++ src/data/bg.js
@@ -3,13 +3,13 @@ export const bg =  [
     id: 1,
     description: "Candidato a edil Usaquén",
     img: "/imgs/test-bg-1.webp",
-    sizes: "1/3 pt-24 md:pt-48"
+    sizes: "1/3 pt-20 md:pt-48"
   },
   {
     id: 2,
     description: "Candidato a edil Usaquén2",
     img: "/imgs/test-bg-2-1.webp",
-    sizes: "1/2 pt-24 md:pt-48"
+    sizes: "1/2 pt-16 md:pt-48"
   },
   {
     id: 3,
